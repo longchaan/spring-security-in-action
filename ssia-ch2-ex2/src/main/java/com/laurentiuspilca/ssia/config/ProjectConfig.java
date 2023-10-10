68cf17b51fae60c22f3b660469cb84cf714a847e
Laurentiu Spilca <ssia@laurspilca.com>
2023-10-10 17:47:34 +0900

    ssia-ch2-ex2: 2장 안녕! 시큐리티 > 2.3 기본 구성 재정의
    ### 2.3.1 UserDetailsService 구성 요소 재정의
    
    ```
    @Configuration
    public class ProjectConfig {
        @Bean
        public UserDetailsService userDetailsService() {
            var userDetailsService = new InMemoryUserDetailsManager();
    
            var user = User.withUsername("john")
                    .password("12345")
                    .authorities("read")
                    .build();
    
            userDetailsService.createUser(user);
    
            return userDetailsService;
        }
    }
    ```
    - org.springframework.security.core.userdetails.User 클래스는 사용자 객체를 만드는 빌더
    구현이다.
    - User 클래스를 이용하여 사용자 이름과 암호에 값을 지정하고 권한에도 하나 이상의 값을 지정해야 한다.
    - 기본 UserDetailsService를 재정의하면 PasswordEncoder도 다시 선언해야 한다.
    
    ```
    curl -u john:12345 http://localhost:8080/hello
    ```
    ```
    java.lang.IllegalArgumentException: There is no PasswordEncoder mapped
    for the id "null"
     at org.springframework.security.crypto.password
     ➥.DelegatingPasswordEncoder$UnmappedIdPasswordEncoder
     ➥.matches(DelegatingPasswordEncoder.java:244)
     ➥~[spring-security-core-5.1.6.RELEASE.jar:5.1.6.RELEASE]
    ```
    ```
        @Bean
        public PasswordEncoder passwordEncoder() {
            return NoOpPasswordEncoder.getInstance();
        }
    ```
    ```
    curl -u john:12345 http://localhost:8080/hello
    Hello!
    ```

--- ssia-ch2-ex2/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
+++ ssia-ch2-ex2/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
@@ -2,7 +2,10 @@ package com.laurentiuspilca.ssia.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -11,6 +14,19 @@ public class ProjectConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         var userDetailsService = new InMemoryUserDetailsManager();
+
+        var user = User.withUsername("john")
+                .password("12345")
+                .authorities("read")
+                .build();
+
+        userDetailsService.createUser(user);
+
         return userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
