d067596600f81e9c33ee4147a67474ef232d7dd8
Laurentiu Spilca <ssia@laurspilca.com>
2023-10-11 11:09:29 +0900

    ssia-ch2-ex3: 2장 안녕! 시큐리티 > 2.3 기본 구성 재정의
    ### 2.3.3 다른 방법으로 구성 설정
    
    - WebSecurityConfigurerAdapter 클래스에는 오버로드된 세 가지 다른 configure 메서드가 있다.
    - 이전 구성에서는 스프링 컨텍스트에 빈으로 추가해 UserDetailsService와 PasswordEncoder를 재정의했다.
    - AuthenticationManagerBuilder의 userDetailsService() 메서드를 호출해서
    userDetailsService 인스턴스 등록했다. 또한 passwordEncoder() 메서드를 호출해서
    PasswordEncoder를 등록했다.
    - 구성을 혼합하면 헷갈릴 수 있어 권장하지 않는다. 예를들어 다음 코드를 보면 UserDetailsService 및
    PasswordEncoder 연결이 어디인지 알기가 쉽지 않다.
    
    ```
    @Configuration
    public class ProjectConfig extends WebSecurityConfigurerAdapter {
    
        @Bean
        public PasswordEncoder passwordEncoder() {
            return NoOpPasswordEncoder.getInstance(); // Designs the
    PasswordEncoder as a bean
        }
    
        @Override
        protected void configure(AuthenticationManagerBuilder auth) throws
    Exception {
            var userDetailsService = new InMemoryUserDetailsManager();
    
            var user = User.withUsername("john")
                    .password("12345")
                    .authorities("read")
                    .build();
    
            userDetailsService.createUser(user);
    
            auth.userDetailsService(userDetailsService) // Configures
    UserDetailsService
                .passwordEncoder(NoOpPasswordEncoder.getInstance()); // and
    PasswordEncoder
        }
    
    }
    ```

---
+++ ssia-ch2-ex3/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
@@ -0,0 +1,41 @@
+package com.laurentiuspilca.ssia.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance(); // Designs the PasswordEncoder as a bean
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        var userDetailsService = new InMemoryUserDetailsManager();
+
+        var user = User.withUsername("john")
+                .password("12345")
+                .authorities("read")
+                .build();
+
+        userDetailsService.createUser(user);
+
+        auth.userDetailsService(userDetailsService) // Configures UserDetailsService 
+            .passwordEncoder(NoOpPasswordEncoder.getInstance()); // and PasswordEncoder
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+}
